2125daea96679a64fe55b706e20879b9ef2fff06
Stefano Furi <stefano.furi@studio.unibo.it>
2021-12-14 15:20:00 +0100

    2 test cases failed for floating point precision loss (0.4999999999999999 instead of 0.5 )

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace ExtensionMethods
 {
     using System;
@@ -5,8 +7,8 @@ namespace ExtensionMethods
     /// <inheritdoc cref="IComplex"/>
     public class Complex : IComplex
     {
-        private readonly double re;
-        private readonly double im;
+        private readonly double _re;
+        private readonly double _im;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Complex"/> class.
@@ -15,71 +17,48 @@ namespace ExtensionMethods
         /// <param name="im">the imaginary part.</param>
         public Complex(double re, double im)
         {
-            this.re = re;
-            this.im = im;
+            _re = re;
+            _im = im;
         }
 
         /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Real => _re;
 
         /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Imaginary => _im;
 
         /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Modulus => Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2));
 
         /// <inheritdoc cref="IComplex.Phase"/>
-        public double Phase
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Phase => Math.Atan2(Real, Imaginary);
 
         /// <inheritdoc cref="IComplex.ToString"/>
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            return String.Format("{0}{1}{2}", Real == 0 ? "" : Real.ToString(),
+                Imaginary <= 0 ? "" : "+",
+                Imaginary == 0 ? "" : (Imaginary.Equals(1) ? "i" :
+                    (Imaginary.Equals(-1) ? "-i" : Imaginary.ToString(CultureInfo.CurrentCulture) + "i")));
         }
 
-        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
+
         public bool Equals(IComplex other)
         {
-            throw new System.NotImplementedException();
+            return _re.Equals(other.Real) && _im.Equals(other.Imaginary);
         }
 
-        /// <inheritdoc cref="object.Equals(object?)"/>
         public override bool Equals(object obj)
         {
-            // TODO improve
-            return base.Equals(obj);
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Complex) obj);
         }
 
-        /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            // TODO improve
-            return base.GetHashCode();
+            return HashCode.Combine(Real, Imaginary);
         }
     }
 }
--- 4-ExtensionMethods/ComplexExtensions.cs
+++ 4-ExtensionMethods/ComplexExtensions.cs
@@ -15,7 +15,7 @@ namespace ExtensionMethods
         /// <returns>the sum.</returns>
         public static IComplex Add(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
         }
 
         /// <summary>
@@ -26,7 +26,7 @@ namespace ExtensionMethods
         /// <returns>the difference.</returns>
         public static IComplex Subtract(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
         }
 
         /// <summary>
@@ -37,7 +37,14 @@ namespace ExtensionMethods
         /// <returns>the product.</returns>
         public static IComplex Multiply(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            var re = (c1.Real * c2.Real) - (c1.Imaginary * c2.Imaginary);
+            var im = (c1.Real * c2.Imaginary) + (c2.Real * c1.Imaginary);
+            return new Complex(re, im);
+        }
+
+        public static IComplex NumMultiply(this IComplex c1, double num)
+        {
+            return new Complex(c1.Real * num, c1.Real * num);
         }
 
         /// <summary>
@@ -48,7 +55,12 @@ namespace ExtensionMethods
         /// <returns>the quotient.</returns>
         public static IComplex Divide(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return c1.Multiply(c2.Reciprocal());
+        }
+
+        public static IComplex NumDivide(this IComplex c1, double num)
+        {
+            return c1.NumMultiply(1.0 / num);
         }
 
         /// <summary>
@@ -64,7 +76,7 @@ namespace ExtensionMethods
         /// <returns>the complex conjugate.</returns>
         public static IComplex Conjugate(this IComplex c1)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real, (-1)*c1.Imaginary);
         }
 
         /// <summary>
@@ -80,7 +92,7 @@ namespace ExtensionMethods
         /// <returns>the complex reciprocal.</returns>
         public static IComplex Reciprocal(this IComplex c1)
         {
-            throw new NotImplementedException();
+            return c1.Conjugate().NumDivide(c1.Modulus * c1.Modulus);
         }
     }
 }
